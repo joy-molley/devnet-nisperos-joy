d227ab4c177993d3332f759c70dd6fbc859084ea
joy-molley <joymalisonhappy@gmail.com>
2026-09-25 22:35:12 +0000

    Add Module 2 Lesson 1 variables and data types

--- module-2-python-basics/lesson-1-variables-datatypes.py
+++ module-2-python-basics/lesson-1-variables-datatypes.py
@@ -1,24 +1,27 @@
 """
 Module 2 — Lesson 1: Variables & Data Types
-Student: [your name]
-Date: [date]
+Student: [Joy Nisperos]
+Date: [September 26, 2026]
 
 ============================================
 WHAT IS THIS TOPIC? (explain it like you're
 teaching a friend who's never coded before)
 ============================================
-[write your own explanation here]
+[variables are like containers where we store information and each variable has a name and a value.
+Data type tells us what kind of information is stored in a variable. For example for 
+whole number we use int, strings for letters or text , float for numbers with decimals 
+and boolean for values with conditions that answers true or false. ]
 
 
 ============================================
 KEY VOCABULARY
 ============================================
-- variable:
-- data type:
-- int:
-- float:
-- string:
-- boolean:
+- variable: a container where we store information
+- data type: tells us what kind of information is stored in the variable container
+- int: use for whole number 
+- float: use for numbers with decimal 
+- string: letters/alphabet or text
+- boolean: use for conditions that answers true or false
 (add more as needed)
 
 
@@ -29,19 +32,30 @@ Write at least one working example below that you
 came up with yourself — not copied from class.
 """
 
-# --- your code example goes here ---
+name = "Joy"
+age = 37
+avarage_grade = 90.50
+is_ student 
+
+print("Name:", name)
+print("Age:", age)
+print("Average Grade:", average_grade)
+print("Currently a student:", is_student)
 
 
 """
 ============================================
 A MISTAKE I MADE (or one I want to avoid)
 ============================================
-[what's something confusing or easy to get wrong
-about this topic?]
+[I usually forget are quotation mark and comma.
+ It is important to check these simple symbols becuase 
+ not putting in quotation mark will make the data type 
+ into another variable.]
 
 
 ============================================
 HOW THIS CONNECTS TO SOMETHING ELSE
 ============================================
-[optional]
+[variables and data types are important because they are used in any type of program.
+They are use with input, calculations, conditions and other functions in programming.]
 """
